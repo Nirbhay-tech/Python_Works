125e9d5f3b9c1121b53fc9258fcbc3ae694fb9da
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-02-09 12:18:11 +0530

    python program for multiline message

---
+++ Displaying Multiline message/multiline_msg.py
@@ -0,0 +1,13 @@
+#comments
+'''This line contains a multiline message
+Then we will display it'''
+
+#creating a multiline message
+
+message='''Hello How are you?
+I am fine.
+What about you?
+I am fine and learning  python '''
+
+#to display it
+print(message)
